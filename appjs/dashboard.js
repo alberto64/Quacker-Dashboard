c25693db6d26b2535f39c9f565a3bc34f01c9f96
alejandra <alejandra.casanova@upr.edu>
2018-05-30 13:22:50 -0400

    setting up application

--- appjs/dashboard.js
+++ appjs/dashboard.js
@@ -6,50 +6,115 @@
 google.charts.load('current', {'packages': ['corechart']});
 
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+//google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawMessageChart);
 
-function reformatData(jsonData){
-    var temp= jsonData.Topics;
+//function reformatData(jsonData){
+//    var temp= jsonData.Topics;
+//    console.log("temp: " + JSON.stringify(temp));
+//    var result = [];
+//    var i;
+//    var row;
+//    for (i=0; i < temp.length; ++i){
+//        row= temp[i]
+//        dataElement = [];
+//        dataElement.push(row.id + '-' + row.name);
+//        dataElement.push(row.count);
+//        result.push(dataElement);
+//    }
+//    console.log("Data: " + JSON.stringify(result));
+//    return result;
+//}
+//function drawChart() {
+//    var jsonData = $.ajax({
+//        url: "http://quacker-pr.herokuapp.com/dashboard/topics",
+//        dataType: "json",
+//        async: false
+//    }).responseText;
+//    console.log(jsonData);
+//    console.log("jsonData: " + JSON.parse(jsonData));
+//
+//    // Create our data table out of JSON data loaded from server.
+//    var data = new google.visualization.DataTable();
+//    data.addColumn('string', 'Parts');
+//    data.addColumn('number', 'Stock');
+//
+//    data.addRows(reformatData(JSON.parse(jsonData)));
+//
+//    var options = {
+//        title: 'Stock Parts by Id/name',
+//        chartArea: {width: '50%'},
+//        hAxis: {
+//            title: 'Total Number',
+//            minValue: 0
+//        },
+//        vAxis: {
+//            title: 'Part'
+//        }
+//    };
+//
+//    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+//
+//    chart.draw(data, options);
+//
+//}
+
+function reformatMessageData(jsonData){
+    var temp= jsonData.Messages;
     console.log("temp: " + JSON.stringify(temp));
+
     var result = [];
     var i;
     var row;
-    for (i=0; i < temp.length; ++i){
+    var keys = Object.keys(temp);
+    console.log(keys);
+    for(i=0; i < keys.length; ++i) {
+        console.log();
+        console.log();
+
+        dataElement.push(keys[i]);
+        dataElement.push(temp[keys[i]][0]);
+    }
+
+     for (i=0; i < temp.length; ++i){
+
+        //console.log("Checking the values of each key")
+        //console.log(key);
+       // console.log(temp[key]);
         row= temp[i]
         dataElement = [];
-        dataElement.push(row.id + '-' + row.name);
-        dataElement.push(row.count);
+
         result.push(dataElement);
-    }
-    console.log("Data: " + JSON.stringify(result));
+
+  }
+//  console.log("Data: " + JSON.stringify(result));
     return result;
 }
-
-function drawChart() {
+function drawMessageChart() {
     var jsonData = $.ajax({
-        url: "http://quacker-pr.herokuapp.com/dashboard/topics",
+        url: "http://quacker-pr.herokuapp.com/dashboard/messages",
         dataType: "json",
         async: false
     }).responseText;
-    console.log(jsonData);
+   // console.log(jsonData);
     console.log("jsonData: " + JSON.parse(jsonData));
 
     // Create our data table out of JSON data loaded from server.
     var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Parts');
-    data.addColumn('number', 'Stock');
-    
-    data.addRows(reformatData(JSON.parse(jsonData)));
+    data.addColumn('string', 'Days');
+    data.addColumn('number', 'messagesPerDay');
+
+    data.addRows(reformatMessageData(JSON.parse(jsonData)));
 
     var options = {
-        title: 'Stock Parts by Id/name',
+        title: 'Messages per day',
         chartArea: {width: '50%'},
         hAxis: {
-            title: 'Total Number',
+            title: 'Total Number of Messasges',
             minValue: 0
         },
         vAxis: {
-            title: 'Part'
+            title: 'Day'
         }
     };
 
@@ -59,8 +124,8 @@ function drawChart() {
 
 }
 
-
 google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawChart);
+//google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawMessageChart);
 
 
